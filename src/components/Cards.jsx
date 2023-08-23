36f38edf21710bff0014ed2bfafe9d20b74db641
Belén Nobili <99286511+Bele208@users.noreply.github.com>
2023-08-23 08:15:17 -0300

    Diseño de Card_details (#6)

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,7 +2,7 @@ import { Link as Anchor} from "react-router-dom"
 
 export default function Cards({src, alt, text, id}) {
   return (
-    <Anchor to={'/cities/'+id}  className="unique-card">
+    <Anchor to={'/cities/' + id}  className="unique-card">
            <img src={src} alt={alt} />
            <p>{text}</p>
     </Anchor>
--- src/pages-1/Details.jsx
+++ src/pages-1/Details.jsx
@@ -1,15 +1,36 @@
-import { Link as Anchor } from "react-router-dom"
-
-import { useParams } from "react-router-dom"
-import Img_construction from "../components/Img_construction";
+import { useEffect, useState } from "react";
+import { Link as Anchor, useParams } from "react-router-dom";
+import Card_details from "../components/Card_details";
+import axios from "axios";
+import apiUrl from "../apiUrl";
 
 export default function Details() {
-  const { city_id } = useParams()
- return (
+  const { city_id } = useParams();
+  const [cityData, setCityData] = useState(null);
+
+  useEffect(() => {
+    axios.get(apiUrl + '/cities/' + city_id)
+      .then(res => {
+        const city = res.data.response;
+        setCityData(city);
+      })
+      .catch(err => console.log(err));
+  }, [city_id]);
+
+  return (
     <div className="details-cont">
-      <Img_construction />
-      <Anchor to={'/cities/'} >Back to cities</Anchor>
-      <p>CityDetail of city {city_id} under construction</p>
+      {cityData && (
+        <Card_details
+          key={cityData._id}
+          src={cityData.photo}
+          alt={cityData.city}
+          text={cityData.city}
+          description={cityData.description}
+          id={cityData._id}
+        />
+      )}
+
+      <Anchor to={'/cities/'}>Back to cities</Anchor>
     </div>
   );
 }
